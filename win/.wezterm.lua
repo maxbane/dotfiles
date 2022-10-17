52008dceb9402cdce31bba5cd7a3d68ae6e20485
Max Bane <max.bane@gmail.com>
2022-10-17 15:38:57 -0400

    Add config for wezterm.

---
+++ win/.wezterm.lua
@@ -0,0 +1,15 @@
+return {
+    color_scheme = "Dracula",
+    default_prog = {"powershell.exe"},
+    font_size = 11.0,
+    hide_tab_bar_if_only_one_tab = true,
+    use_fancy_tab_bar = false,
+    tab_bar_at_bottom = false,
+    window_padding = {
+        left = 2, 
+        right = 2,
+        top = 2,
+        bottom = 2,
+    },
+    window_decorations = "RESIZE",
+}
